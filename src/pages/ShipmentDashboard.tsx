3e95523e00fd2a754fecf72a19644b9a5114dc13
hm-aditya <patilad3618@gmail.com>
2026-01-29 17:12:18 +0530

    added some comments

--- src/pages/ShipmentDashboard.tsx
+++ src/pages/ShipmentDashboard.tsx
@@ -64,7 +64,7 @@ const ShipmentDashboard = () => {
 
       <div className="flex flex-wrap justify-center gap-2 mb-6">
 
-        //status tabs 
+        {/*status tabs" */}
         {STATUS_TABS.map((status) => {
           const isActive = activeStatus === status;
 
@@ -86,7 +86,7 @@ const ShipmentDashboard = () => {
       </div>
       <ul className="space-y-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          //shipment cards
+  {/*shipment cards*/}
           {filteredShipments.map((shipment) => (
             <li key={shipment.id}>
               <ShipmentCard shipment={shipment} />
